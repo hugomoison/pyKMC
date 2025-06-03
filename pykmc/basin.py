f5f7b0a96f231fc9390665fbf9c42ab4914f6279
Marie-EveD <marie-eve.dussault.1@umontreal.ca>
2025-06-03 13:58:17 +0000

    Fonction detectin

--- pykmc/basin.py
+++ pykmc/basin.py
@@ -1,10 +1,33 @@
+from kmc import KMC
+
+
+
 class Basin() : 
 
-    
+
+    def detectin(selected_active_event_series) : 
+
+        dE_forward = selected_active_event_series['energy_barrier']
+        dE_backward = selected_active_event_series['backward_energy_barrier']
+
+        if dE_forward > 0.15  and dE_backward > 0.15 :
+            print ('Basin not detected')
+            return False
+        
+        else :
+            print('Basin detected')
+            return True
 
 
-    def detectin() : 
-        pass 
+
+
+
+
+
+
+
+
+
 
     def execute() : 
         pass 
