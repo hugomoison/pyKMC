ec6735a51354655340862374d0d57ec242adbcdb
Stephen Kerr <24jrpc@queensu.ca>
2026-05-29 16:11:21 -0400

    Add HTST manager/session/engine wiring for per-event prefactors

--- pykmc/enginemanager/lmpi/engines/mpi_api_engine.py
+++ pykmc/enginemanager/lmpi/engines/mpi_api_engine.py
@@ -2,7 +2,7 @@ from lammps import lammps
 import threading 
 from mpi4py import MPI 
 import queue 
-from ..lammps_operations import initialize_parameters, initialize_system, initialize_potential, minimize, get_total_energy, get_positions, set_positions, partn_search, partn_refine, minimize_with_results, get_potential_energy
+from ..lammps_operations import initialize_parameters, initialize_system, initialize_potential, minimize, get_total_energy, get_positions, set_positions, partn_search, partn_refine, minimize_with_results, get_potential_energy, compute_event_prefactors
 from ...messenger import QueueMessenger, MpiMessenger
 
 class MpiApiEngine() : 
@@ -54,8 +54,9 @@ class MpiApiEngine() :
             "set_positions": set_positions, 
             "partn_search": partn_search, 
             "partn_refine" : partn_refine, 
-            "minimize_with_results" : minimize_with_results, 
-            "get_potential_energy" : get_potential_energy
+            "minimize_with_results" : minimize_with_results,
+            "get_potential_energy" : get_potential_energy,
+            "compute_event_prefactors": compute_event_prefactors,
         }
 
 
